9dc23a2cecad1b10cc64e00f46585d3efb6e298e
vivianli <viviannnlee@163.com>
2022-01-29 11:51:34 -0500

    Updates the pull request

---
+++ api/utils.py
@@ -0,0 +1,24 @@
+def solid_example_1(*, example_param_1: str, example_param_2: int):
+    """
+
+    :param example_param_1: this is the first param
+    :param example_param_2: this is the second param
+    :return:
+    """
+    #TODO: THIS IS A TODO. you mush do this.
+
+def solid_example_2(example_param_1: float) -> int:
+    """
+
+    :param example_param_1:
+    :return:
+    """
+    return 2
+
+def solid_example_3(*, example_param_1: float = 4.0):
+    """
+
+    :param example_param_1:
+    :return:
+    """
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from api import utils
+
 def main_wrapper():
     print(f"Hi this is the start. The name is {main_wrapper.__name__}")
+    utils.solid_example_1(example_param_1="a", example_param_2=1)
 
     print("This is the end")
 
